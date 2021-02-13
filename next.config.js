6cb72c2d4fc14dd2da06d309ae4971f4a19a5b47
Mário Menezes <mario082@gmail.com>
2021-02-13 11:22:57 -0300

    config

--- next.config.js
+++ next.config.js
@@ -1,5 +1,5 @@
 module.exports = {
     env: {
-        MONGO_URI: "mongodb+srv://mario:Goprohero5@cluster0.6dafq.mongodb.net/sample_mflix?retryWrites=true&w=majority"
+        MONGO_URI: ""
     }
 }
